751bab1dc520e1bf253a5cb50051c8101d902e20
yjinn0719 <yjinn0719@gmail.com>
2024-04-02 19:04:34 +0900

    💄design : 스타일 전역 변수
    색상 변수 설정

---
+++ src/styles/Theme.jsx
@@ -0,0 +1,15 @@
+export const Theme = {
+  color: {
+    green: '#4F8337',
+    skyblue: '#7DB8E8',
+    beige: '#A99283',
+    lightbeige: '#FFF9F5',
+    gray: '#6E6E6E',
+    lightgray: '#EFEFEF',
+    mediumgray: '#4f4f4f',
+    darkgray: '#383838',
+    white: '#fff',
+  },
+};
+
+export default Theme;

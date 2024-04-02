1937a2eee47fa1c385231a97bcbb11f88bbfd23e
yjinn0719 <yjinn0719@gmail.com>
2024-04-02 19:37:33 +0900

    ✨feat : main에 스타일 전역 변수 적용

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import GlobalStyle from './styles/GlobalStyles';
+import ThemeProvider from 'styled-components';
+import Theme from '../styles/Theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider Theme={Theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );

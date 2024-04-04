bc1715b910b50e76f190baccffc4d1f575349de9
Jeongmin CHOI <141547885+ioimmini@users.noreply.github.com>
2024-04-05 00:58:01 +0900

    ✨feat : ReviewDetail 페이지 구현

---
+++ src/pages/ReviewDetail/ReviewDetail.styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ReviewContainer = styled.div`
+  width: 100%;
+  max-width: 480px;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 66px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+`;
+
+export { ReviewContainer };

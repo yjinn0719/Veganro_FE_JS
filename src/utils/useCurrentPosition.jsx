b03da87d9865585267b86884f3f60daa5e9c809e
Saeyoung Choi <effysogood@gmail.com>
2024-04-06 16:32:12 +0900

    ♻️refactor : useCurrentPositon 리팩토링

--- src/utils/useCurrentPosition.jsx
+++ src/utils/useCurrentPosition.jsx
@@ -1,30 +1,43 @@
+import { useEffect, useState } from 'react';
+
 // 현재 위치 받아오는 함수
 export const getCurrentPosition = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setCenterMove({
-          ...centerMove,
-          center: {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          },
-          isLoading: false,
-        });
-      },
-      (err) => {
-        setCenterMove({
-          ...centerMove,
-          errMsg: err.message,
-          isLoading: false,
-        });
-      },
-    );
-  } else {
-    setCenterMove({
-      ...centerMove,
-      errMsg: '현재 위치를 불러올 수 없습니다.',
-      isLoading: false,
-    });
-  }
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    const { geolocation } = navigator;
+
+    if (geolocation) {
+      geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLocation({
+            ...location,
+            center: {
+              lat: latitude,
+              lng: longitude,
+            },
+            isLoading: false,
+          });
+        },
+        (err) => {
+          setLocation({
+            ...location,
+            errMsg: err.message,
+            isLoading: false,
+          });
+        },
+      );
+    } else {
+      setLocation({
+        ...location,
+        errMsg: '현재 위치를 불러올 수 없습니다.',
+        isLoading: false,
+      });
+    }
+  }, []);
+
+  return {
+    currentLocation: location,
+  };
 };

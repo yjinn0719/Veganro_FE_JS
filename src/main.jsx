415f01c6281b5cbe89b438d06e72b6bfa2335940
Saeyoung Choi <effysogood@gmail.com>
2024-04-06 16:29:51 +0900

    🔧chore : recoil 라이브러리 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { RecoilRoot } from 'recoil';
 import GlobalStyle from './styles/GlobalStyles';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
@@ -10,12 +9,10 @@ import Theme from './styles/Theme';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
-      <RecoilRoot>
-        <ThemeProvider theme={Theme}>
-          <GlobalStyle />
-          <App />
-        </ThemeProvider>
-      </RecoilRoot>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </React.StrictMode>
   </BrowserRouter>,
 );

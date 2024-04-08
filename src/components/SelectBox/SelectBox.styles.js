031ce479088bbaa50ba3210ad7bbda6ed0a552bc
yjinn0719 <yjinn0719@gmail.com>
2024-04-08 21:31:36 +0900

    🚚rename : 폴더 및 파일 이름 변경
    ShortInputBox -> SelectBox

--- src/components/SelectBox/SelectBox.styles.js
+++ src/components/SelectBox/SelectBox.styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const ShortInputContainer = styled.div`
+export const SelectBoxContainer = styled.div`
   height: 50px;
   padding: 12px;
   align-items: center;

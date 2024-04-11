f3f39f0d603f624e2188745448124a53bb9a883c
yjinn0719 <yjinn0719@gmail.com>
2024-04-11 22:10:55 +0900

    ✨feat : 가게 제보 api 연결

--- src/components/KakaoAddress/KakaoAddress.jsx
+++ src/components/KakaoAddress/KakaoAddress.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DaumPostcode from 'react-daum-postcode';
 import { ModalOverlay, ModalContent } from './KakaoAddress.styles';
 
 const KakaoAddress = ({ onClose, onAddressSelect }) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
   const handleComplete = (data) => {
     let fullAddress = data.address;
     let extraAddress = '';
@@ -19,6 +26,8 @@ const KakaoAddress = ({ onClose, onAddressSelect }) => {
     }
     // 선택한 주소 정보를 부모 컴포넌트로 전달
     onAddressSelect(fullAddress);
+    // 모달을 닫을 때 body의 스크롤을 다시 허용
+    document.body.style.overflow = 'auto';
     onClose();
   };
 

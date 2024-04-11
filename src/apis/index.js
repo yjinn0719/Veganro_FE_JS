6665e73cdb24fc08c663ed44cd3d27f5aaa5d3bf
Saeyoung Choi <effysogood@gmail.com>
2024-04-11 17:11:34 +0900

    🚚rename : api 디렉토리 경로 수정

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,5 @@
 import api from './utils/axiosInstance';
-import { fetchPlaces } from './api/placeApi';
+import { getAllPlaces, fetchPlaces } from './api/placeApi';
 import getPlacesWithDistance from './service/distance';
 
-export { api, fetchPlaces, getPlacesWithDistance };
+export { api, getAllPlaces, fetchPlaces, getPlacesWithDistance };

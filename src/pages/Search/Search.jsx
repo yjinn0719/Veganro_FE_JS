8ec757efd4b865644b5cda7d6f932802b88e6a0e
Chanjoo Park <cjangel0503@gmail.com>
2024-04-12 13:51:02 +0900

    💄design : SearchNav 완성

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -6,13 +6,21 @@ import { getPlacesWithDistance } from '@/apis/index';
 import SearchBar from '@/components/SearchBar/SearchBar';
 import PlaceCategory from '@/components/PlaceCategory/PlaceCategory';
 import SearchList from '@/components/SearchList/SearchList';
-import { Box, Wrapper, SearchNav } from './Search.style';
+import SmallRoundButton from '@/components/SmallRoundButton/SmallRoundButton';
+import {
+  Categories,
+  Wrapper,
+  SearchNav,
+  FilterBar,
+  FilterButton,
+} from './Search.style';
 
 import { PLACE_TYPES } from '../../constants';
 
 export default function Search() {
   const [places, setPlaces] = useState([]);
   const { location, isLoading, error } = useCurrentLocation(); // 사용자~place 동적 거리계산 상태관리용
+  const [showFilterModal, setShowFilterModal] = useState(false); //recoil로 옮겨야하는 상태관리값
 
   useEffect(() => {
     // 추후 리팩터링 때 try-catch 검증 추가
@@ -35,12 +43,28 @@ export default function Search() {
     }
   }, [location, isLoading]);
 
+  // 카테고리 초기화 핸들러
+  const handleCategoryReset = () => {
+    setCategoriesStatus((prevCategories) =>
+      prevCategories.map((category) => ({
+        ...category,
+        clicked: 0,
+      })),
+    );
+  };
+
+  // 필터 모달 핸들러
+  const handleFilterModal = () => {
+    setShowFilterModal(!showFilterModal);
+    console.log('필터 모달');
+  };
+
   if (error) {
-    return <div>위치 정보 로딩 에러ㅜㅜ {error}</div>;
+    return <div>위치 정보를 가져올 수 없습니다 {error}</div>;
   }
 
   if (isLoading) {
-    return <div>위치 정보 로딩중...</div>;
+    return <div>위치 정보를 가져오는 중..</div>;
   }
 
   return (
@@ -48,15 +72,16 @@ export default function Search() {
       <Wrapper className="search">
         <SearchNav>
           <SearchBar placeholder="‘가게 이름' 또는 ‘주소'를 검색해보세요." />
-          <Box className="category-bar">
-            {PLACE_TYPES.map((title, index) => (
-              <PlaceCategory
-                key={index}
-                title={title}
-                onClick={() => handleCategorySelect(title)}
-              />
-            ))}
-          </Box>
+          <FilterBar>
+            <Categories className="category-bar">
+              {PLACE_TYPES.map((title, index) => (
+                <PlaceCategory key={index} title={title} onClick={() => {}} />
+              ))}
+              <SmallRoundButton title="refresh" onClick={handleCategoryReset} />
+            </Categories>
+            <FilterButton title="filter" onClick={handleFilterModal} />
+            {showFilterModal && <MapFilterModal />}
+          </FilterBar>
         </SearchNav>
         <div>
           {places &&

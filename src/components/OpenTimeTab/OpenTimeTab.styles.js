5aa758e45455b17d05193ccf22379aabbe58f895
yjinn0719 <yjinn0719@gmail.com>
2024-04-13 11:19:59 +0900

    ✨feat : OpenTimeTab 컴포넌트 추가

---
+++ src/components/OpenTimeTab/OpenTimeTab.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const OpenTimeTabContainer = styled.div``;
+
+export const DateTagContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  margin-bottom: 12px;
+`;
+
+export const SelectBoxContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  margin-bottom: 12px;
+  width: 100%;
+  justify-content: space-between;
+`;

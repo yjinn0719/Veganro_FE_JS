d87e7ed9c0e7ebd6da6172abfbd22c76055ddfa9
yjinn0719 <yjinn0719@gmail.com>
2024-04-14 17:18:10 +0900

    ✨feat : 소셜로그인 페이지 및 기능 완료

---
+++ src/pages/Auth/Auth.styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const AuthContainer = styled.div`
+  display: inline-flex;
+  width: 100%;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--Gray-900, #2d2d2d);
+  gap: 48px;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const LogoContent = styled.p`
+  color: #fff;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 0.48px;
+`;

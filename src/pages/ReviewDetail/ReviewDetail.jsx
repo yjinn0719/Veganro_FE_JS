8d068d1293795bea1a9db3bfad56cfdcf0ed7884
Jeongmin CHOI <141547885+ioimmini@users.noreply.github.com>
2024-04-16 18:23:06 +0900

    💡comment : console.log 정리

--- src/pages/ReviewDetail/ReviewDetail.jsx
+++ src/pages/ReviewDetail/ReviewDetail.jsx
@@ -54,7 +54,6 @@ export default function Review() {
   }, [isReviewDrawerOpen]);
   useEffect(() => {
     if (inView && hasNextPage && !isFetchingNextPage) {
-      console.log(inView, hasNextPage, isFetchingNextPage);
       fetchNextPage();
     }
   }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);

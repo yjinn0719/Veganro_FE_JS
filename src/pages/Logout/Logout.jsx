d41e70cdf7bce6db496dd14dc35cd1f7357ef467
yjinn0719 <yjinn0719@gmail.com>
2024-04-19 22:58:08 +0900

    ✨feat : 비회원시 로그아웃 버튼 막기

--- src/pages/Logout/Logout.jsx
+++ src/pages/Logout/Logout.jsx
@@ -1,19 +1,22 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { notify } from '../../hooks/useToast'; // notify 함수를 임포트합니다.
 
 const Logout = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const confirmLogout = window.confirm('정말 로그아웃 하시겠습니까?');
-    if (confirmLogout) {
-      const token = localStorage.getItem('Authorization');
-      if (token) {
-        localStorage.removeItem('Authorization');
-      }
+    const token = localStorage.getItem('Authorization');
+    if (!token) {
+      notify('error', '로그아웃하실 수 없습니다. 로그인 먼저 시도해주세요.');
       navigate('/');
     } else {
-      navigate('/home');
+      if (window.confirm('정말 로그아웃 하시겠습니까?')) {
+        localStorage.removeItem('Authorization');
+        navigate('/');
+      } else {
+        navigate('/home');
+      }
     }
   }, [navigate]);
 
